4d3d7ec8e5493c809cf006626ee0596f8058c12a
garytcfsh134 <garytcfsh@gmail.com>
2020-03-31 12:58:38 +0800

    bug, input region file

--- fixhybriddefinition.cpp
+++ fixhybriddefinition.cpp
@@ -2,33 +2,41 @@
 
 FixHybridDefinition::FixHybridDefinition()
 {
-//    QString regionPath, definitionPath;
-//    qDebug()<< "select input region type: 1. box_region 2. slab_region" << endl;
-//    QTextStream s( stdin);
-//    QString regionType = s.readLine();
-//    if (regionType == "1")
-//        inputBoundaryRegion = new BoxRegion();
-//    else if (regionType == "2")
-//        inputBoundaryRegion = new SlabRegion();
-//    else
-//    {
-//        qDebug()<< "region type error" << endl;
-//        exit(2);
-//    }
-//    qDebug()<< "region file (.sab): ";
-//    QTextStream sr( stdin);
-//    regionPath = sr.readLine();
-//    inputBoundaryRegion->setRegionFace( regionPath, regionFace);
-//    qDebug()<< "preliminary head file (restart.mff): ";
-//    QTextStream s1( stdin);
-//    QString restartPath = s1.readLine();
-//    searchNodeOnRegionFace = new StoreRestartFileNodeOnRegionFace();
-//    searchNodeOnRegionFace->storeNodeOnRegionFace( restartPath, regionFace, disError, degError);
-
+    //merge file
+    qDebug()<< "dfn definition file (.mff)";
     QTextStream d( stdin);
     QString dfnPath = d.readLine();
+    qDebug()<< "hybrid definition file (.mff)";
     QTextStream h( stdin);
     QString hybridPath = h.readLine();
     mergefile = new DFN2Hybrid();
     mergefile->merge( dfnPath, hybridPath);
+
+    //read region file
+    QString regionPath;
+    qDebug()<< "select input region type: 1. box_region 2. slab_region" << endl;
+    QTextStream s( stdin);
+    QString regionType = s.readLine();
+    if (regionType == "1")
+        inputBoundaryRegion = new BoxRegion();
+    else if (regionType == "2")
+        inputBoundaryRegion = new SlabRegion();
+    else
+    {
+        qDebug()<< "region type error" << endl;
+        exit(2);
+    }
+    qDebug()<< "region file (.sab): ";
+    QTextStream sr( stdin);
+    regionPath = sr.readLine();
+    inputBoundaryRegion->setRegionFace( regionPath, regionFace);
+
+    //change boundary condition of nodes
+    qDebug()<< "hybrid definition file (.mff): ";
+    disError = 1;
+    degError = 1;
+    hybridPath = hybridPath + "merge";
+    searchNodeOnRegionFace = new StoreDefinitionFileNodeOnRegionFace();
+    searchNodeOnRegionFace->storeNodeOnRegionFace( hybridPath, regionFace, disError, degError);
+
 }
